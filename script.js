72b510155c71dc7dea07e43704144a5a59f820f5
Prabhjeet-kaur98 <122941904+Prabhjeet-kaur98@users.noreply.github.com>
2024-02-26 15:43:22 -0500

    Add code for cards and javascript for button function

--- script.js
+++ script.js
@@ -34,3 +34,30 @@ showSlide(currentSlideIndex);
 
 setInterval(nextSlide, 3000);
 
+
+
+
+function filterProducts(category) {
+    var cards = document.querySelectorAll('.product-card');
+    var buttons = document.querySelectorAll('.filter-btn');
+  
+    cards.forEach(function(card) {
+      card.style.display = card.classList.contains(category) ? 'block' : 'none';
+    });
+  
+    buttons.forEach(function(button) {
+      if (button.textContent.toLowerCase().includes(category)) {
+        button.classList.add('active');
+      } else {
+        button.classList.remove('active');
+      }
+    });
+  }
+  
+  // Event listener for DOMContentLoaded to set default filter
+  document.addEventListener('DOMContentLoaded', function() {
+    filterProducts('new'); // Set 'new' as the default filter
+  });
+  
+  
+
