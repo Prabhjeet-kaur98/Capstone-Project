99716eb84034653dccd32bf2ae1de0205e8c4195
Prabhjeet-kaur98 <122941904+Prabhjeet-kaur98@users.noreply.github.com>
2024-02-26 11:28:21 -0500

    Add function to the crousel and fix navbar function

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
 function toggleMenu() {
     const navLinks = document.querySelector('.nav-links');
     const overlay = document.querySelector('.overlay');
-
-    if (navLinks.style.right === "0px") {
-        navLinks.style.right = "-100%";
-        overlay.style.display = "none";
-    } else {
-        navLinks.style.right = "0";
-        overlay.style.display = "block";
-    }
+    navLinks.classList.toggle('active');
+    overlay.classList.toggle('active');
 }
+// Function for crousel slides
+let currentSlideIndex = 0;
+
+function showSlide(index) {
+  const slides = document.querySelectorAll('.slide');
+  if (index >= slides.length) index = 0;
+  if (index < 0) index = slides.length - 1;
+
+  for (let slide of slides) {
+    slide.style.display = 'none';
+  }
+
+  slides[index].style.display = 'block';
+  currentSlideIndex = index;
+}
+
+function nextSlide() {
+  showSlide(currentSlideIndex + 1);
+}
+
+function previousSlide() {
+  showSlide(currentSlideIndex - 1);
+}
+
+// Initially display the first slide
+showSlide(currentSlideIndex);
+
+
+setInterval(nextSlide, 3000);
+
